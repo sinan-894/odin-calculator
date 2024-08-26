2eb917381811e29709694f1bc963346c8f0ee4d1
sinan-894 <mohammedsinan1011@gmail.com>
2024-08-26 15:33:21 +0530

    Complete calculator functions

--- calculator.js
+++ calculator.js
@@ -19,19 +19,19 @@ function divide(a,b){
 }
 
 
-function operate(firstNumber,secovdVumer,operator){
+function operate(firstNumber,secondNumber,operator){
     switch (operator){
         case "+":
-            return add(firstNumber,secovdVumer);
+            return add(firstNumber,secondNumber);
             break;
         case "-":
-            return subtract(firstNumber,secovdVumer);
+            return subtract(firstNumber,secondNumber);
             break;
         case "*":
-            return multiply(firstNumber,secovdVumer);
+            return multiply(firstNumber,secondNumber);
             break;
         case "/":
-            return divide(firstNumber,secovdVumer);
+            return divide(firstNumber,secondNumber);
             break;
         default:
             console.log("enter valid operator");
@@ -40,14 +40,58 @@ function operate(firstNumber,secovdVumer,operator){
     }
 }
 
+function calcAndDisplayResult(){
+    secondNumber = Number(resultBox.textContent);
+    let result = operate(firstNumber,secondNumber,operator);
+    resultBox.textContent = `${result}`;
+    //setting result to firstNumber will allow calculation of more than single pair
+    firstNumber = result;
+}
+
+
+const operatorStr = '+-*/';
+let firstNumber = undefined
+let secondNumber = undefined
+let operator = undefined
 const buttonList = Array.from(document.querySelectorAll("button"));
-const vumerList = buttonList.filter((btn)=>("0123456789".includes(btn.textContent)))
-console.log(vumerList);
-vumerList.map((btn)=>{
+const numberList = buttonList.filter((btn)=>("0123456789".includes(btn.textContent)))
+const resultBox = document.querySelector('.display');
+let isNumberNotEditable = true;
+clearResultBox = (str)=>resultBox.textContent = str;
+console.log(numberList);
+numberList.map((btn)=>{
     btn.addEventListener('click',()=>{
+        if(isNumberNotEditable){
+            clearResultBox("");
+            isNumberNotEditable = false
+        }
+        resultBox.textContent+=btn.textContent
+    })
+})
+const operatorList = buttonList.filter((btn)=>!("0123456789".includes(btn.textContent)))
+operatorList.map((btn)=>{
+    btn.addEventListener('click',()=>{
+        if(operatorStr.includes(btn.textContent)){
+            if (operator===undefined){
+                firstNumber = Number(resultBox.textContent);
+            }
+            else{
+                calcAndDisplayResult();
+            }
+            operator = btn.textContent;
+        }
+        else if(btn.textContent==='='){
+            calcAndDisplayResult()
+            operator = undefined;
+        }
+        else{
+            clearResultBox('0');
+        }
+        isNumberNotEditable=true;
         console.log(btn.textContent)
     })
 })
+
 
 
 
